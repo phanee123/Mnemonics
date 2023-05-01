c6a19ec834249e15025c05d4212f8d44f45c4d15
Phaneendra Gajawada <phaneendra.gajawada@infor.com>
2023-05-01 10:16:44 +0530

    added correct instructions

--- src/constants/InstructionSteps.js
+++ src/constants/InstructionSteps.js
@@ -5,56 +5,68 @@ const INSTRUCTION_STEPS_TEST1 = [
     stepIndex: 1,
     content: `<h2>Welcome to the experiment!</h2>
           <p>
-            In our day to day life, numerous stimuli act upon our sense organs. Yet we notice and attend to very few
-            objects or events. Hence, Attention is defined as the act of focusing our consciousness on an object or an
-            event.
-          </p>
-          <p>
-            In this experiment, we will try to understand how much we can grasp in one short presentation with different
-            types of stimuli, such as, dots, words, nonsense syllables and numbers
+          Welcome to the experiment!
+          {write some basic intro for page 1 on mnemonics same will be translated and written in telugu}
           </p>`,
     buttonLabel2: "Next",
   },
   {
     stepIndex: 2,
     content: `
-          <h2>Instructions</h2>
+          <h2>Precautions</h2>
           <p>
-            In this experiment, to study span of attention five different types of visual stimuli/ conditions will be
-            presented.
+          1.The student/ subject has to sit 1 ft away from the screen.
+
+          విద్యార్థి/ సబ్జెక్ట్ స్క్రీన్ నుండి 1-2 అడుగుల దూరంలో కూర్చోవాలి 
+          
           </p>
-          <p>Each condition will be administered separately.</p>
-          <p>And, instructions are given separately for each condition.</p>
-          <p>*For Demo purposes we are only presenting one condition.</p>`,
+          <p>2.The subject/ student has to sit in an isolated room so that he/she will not get distracted.
+
+          సబ్జెక్ట్/విద్యార్థి వివిక్త గదిలో కూర్చోవాలి, తద్వారా అతను/ఆమె దృష్టి మరల్చకూడదు.
+          </p>
+          <p>3.If the subject is using phone/ tab then its mandatory to put the phone on DND mode.
+
+          విద్యార్థి ఫోన్‌లు/ట్యాబ్‌లను ఉపయోగిస్తుంటే, ఫోన్‌ను DND మోడ్‌లో ఉంచడం తప్పనిసరి.
+          </p>`,
     buttonLabel1: "Previous",
     buttonLabel2: "Next",
   },
   {
     stepIndex: 3,
-    content: `<p>Experiment will start after you end instructions.</p>`,
-    buttonLabel1: "Back",
-    buttonLabel2: "End Instructions",
+    content: `
+    <h2>Instructions</h2>
+    <p>A set of words will be presented to you on the screen, one after the other. Each word will be 
+    displayed for a brief period of time. Pay attention to them carefully as you will have to recall 
+    them later. </p>
+    <p>{Translate In telugu}</p>`,
+    buttonLabel1: "Previous",
+    buttonLabel2: "Next",
   },
 ];
+
+//second set of instructions which will be displayed before the second trail
 
 const INSTRUCTION_STEPS_TEST2 = [
   {
     stepIndex: 1,
-    content: `<p>Test2 Instructions</p><p>Only colors no text</p`,
+    content: `
+    <h2>Instructions</h2>
+    <p>Same set of words will be presented on the screen. 
+    Please pay attention to them carefully as you will have to recall them later</p><p>Only colors no text</p>
+    <p>{Translate In telugu}</p>,
+    <p> Click next below to move start the trial</p>`,
     buttonLabel2: "Next",
-  },
-  {
-    stepIndex: 2,
-    content: `<p>Experiment will start after you end instructions.</p>`,
-    buttonLabel1: "Previous",
-    buttonLabel2: "End Instructions",
   },
 ];
 
 const INSTRUCTION_STEPS_TEST3 = [
   {
     stepIndex: 1,
-    content: `<p>Test3 Instructions</p><p>Colors out of sync with Text</p`,
+    content: `
+    <h2>Instructions</h2>,
+    <p>Same set of words will be presented on the screen. Please pay attention to them carefully 
+    as you will have to recall them later. </p>
+    <p>{Translate In telugu}</p`,
     buttonLabel2: "Next",
   },
   {
@@ -65,4 +77,8 @@ const INSTRUCTION_STEPS_TEST3 = [
   },
 ];
 
-export { INSTRUCTION_STEPS_TEST1, INSTRUCTION_STEPS_TEST2, INSTRUCTION_STEPS_TEST3 };
+export {
+  INSTRUCTION_STEPS_TEST1,
+  INSTRUCTION_STEPS_TEST2,
+  INSTRUCTION_STEPS_TEST3,
+};
--- src/constants/general.js
+++ src/constants/general.js
@@ -1,6 +1,23 @@
 import ball from "./../assets/fonts/BALL.jpg";
 import basket from "./../assets/fonts/BASKET.jpg";
 import book from "./../assets/fonts/BOOK.jpg";
+import coffee from "./../assets/fonts/COFFEE.jpg";
+import college from "./../assets/fonts/COLLEGE.jpg";
+import comb from "./../assets/fonts/COMB.jpg";
+import confused from "./../assets/fonts/CONFUSED.jpg";
+import doubble from "./../assets/fonts/DOUBBLE.jpg";
+import ee from "./../assets/fonts/EE.jpg";
+import farm from "./../assets/fonts/FARM.jpg";
+import fire from "./../assets/fonts/FIRE.jpg";
+import fishy from "./../assets/fonts/FISHY.jpg";
+import game from "./../assets/fonts/GAME.jpg";
+import light from "./../assets/fonts/LIGHT.jpg";
+import scatter from "./../assets/fonts/SCATTER.jpg";
+import scribble from "./../assets/fonts/SCRIBBLE.jpg";
+import sttammer from "./../assets/fonts/STTAMMER.jpg";
+import tall from "./../assets/fonts/TALL.jpg";
+import umbrella from "./../assets/fonts/UMBRELLA.jpg";
+import zigzag from "./../assets/fonts/ZIGZAG.jpg";
 
 const imageImports = [
   {
@@ -17,6 +34,92 @@ const imageImports = [
     id: 3,
     name: "BOOK",
     imageUrl: book,
+  },
+  {
+    id: 4,
+    name: "COFFEE",
+    imageUrl: coffee,
+  },
+  {
+    id: 5,
+    name: "COLLEGE",
+    imageUrl: college,
+  },
+  {
+    id: 6,
+    name: "COMB",
+    imageUrl: comb,
+  },
+  {
+    id: 7,
+    name: "CONFUSED",
+    imageUrl: confused,
+  },
+  {
+    id: 8,
+    name: "DOUBBLE",
+    imageUrl: doubble,
+  },
+  {
+    id: 9,
+    name: "EE",
+    imageUrl: ee,
+  },
+  {
+    id: 10,
+    name: "FARM",
+    imageUrl: farm,
+  },
+  {
+    id: 11,
+    name: "FIRE",
+    imageUrl: fire,
+  },
+  {
+    id: 12,
+
+    name: "FISHY",
+    imageUrl: fishy,
+  },
+  {
+    id: 13,
+    name: "GAME",
+    imageUrl: game,
+  },
+  {
+    id: 14,
+    name: "LIGHT",
+    imageUrl: light,
+  },
+  {
+    id: 15,
+    name: "SCATTER",
+    imageUrl: scatter,
+  },
+  {
+    id: 16,
+    name: "SCRIBBLE",
+    imageUrl: scribble,
+  },
+  {
+    id: 17,
+    name: "STTAMMER",
+    imageUrl: sttammer,
+  },
+  {
+    id: 18,
+    name: "TALL",
+    imageUrl: tall,
+  },
+  {
+    id: 19,
+    name: "UMBRELLA",
+    imageUrl: umbrella,
+  },
+  {
+    id: 20,
+    name: "ZIGZAG",
+    imageUrl: zigzag,
   },
 ];
 
